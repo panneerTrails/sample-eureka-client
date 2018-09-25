42de50f82643c6e9e7aabf1cc2f0b4749c25ab1b
Panneerselvam <panneerselvam.nithiyanantham@merrillcorp.com>
2018-09-25 18:14:37 +0530

    Initial commit

---
+++ src/main/java/com/sample/eureka/client/SampleEurekaClientApplication.java
@@ -0,0 +1,12 @@
+package com.sample.eureka.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleEurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleEurekaClientApplication.class, args);
+	}
+}
